8b171345de52fd341f5ce49eeddc1c608d96fa1c
romanomatthew23 <mmroman2@illinois.edu>
2016-10-14 10:44:53 -0500

    Added 5 second delay to taking images

--- Experiments/Computer/snapPic.sh
+++ Experiments/Computer/snapPic.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-avconv -f video4linux2 -s 640x480 -i /dev/video1 -ss 0:0:2 -frames 1 E1_data/images/pic_"$1".jpg
+avconv -f video4linux2 -s 640x480 -i /dev/video1 -ss 0:0:5 -frames 1 E1_data/images/pic_"$1".jpg
