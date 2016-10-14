c6466817a130a344520f5ee2cd68722a2be5c24d
romanomatthew23 <mmroman2@illinois.edu>
2016-10-14 10:43:21 -0500

    Update obtainTemps.py

--- Experiments/BBB/obtainTemps.py
+++ Experiments/BBB/obtainTemps.py
@@ -13,12 +13,12 @@ R2 = 9980 # Res for Therm 2 (Ohms)
 #Read from the TMP36 for Air Temperature
 value = ADC.read(TMP36)
 value = ADC.read(TMP36) #the library has a bug that requires sampling twice
-voltage = value * 1.8 #1.8V
-tempC = (voltage - 0.5) * 100 #convert from 10mv/deg with 500mV offset
+TMPvIn = value * 1.8 #1.8V
+tempC = (TMPvIn - 0.5) * 100 #convert from 10mv/deg with 500mV offset
 tempF = (tempC * 9.0 / 5.0) + 32.0
 #print "value = %f" % value
-#print "voltage = %f" % voltage
-print "Air Temp (deg C) = %f" % tempC
+#print "voltage = %f" % TMPvIn
+#print "Air Temp (deg C) = %f" % tempC
 #print "temp (deg F) = %f" % tempF
 
 #Read from Thermistor #1
@@ -26,12 +26,20 @@ value = ADC.read(Th1)
 value = ADC.read(Th1) #the library has a bug that requires sampling twice
 vIn = value * 1.8 #1.8V
 RTh1 = vIn * R1 / (1.8 - vIn)
-print "RTh1 = %f" % RTh1
+#print "RTh1 = %f" % RTh1
 
 #Read from Thermistor #2
 value = ADC.read(Th2)
 value = ADC.read(Th2) #the library has a bug that requires sampling twice
 vIn = value * 1.8 #1.8V
 RTh2 = vIn * R2 / (1.8 - vIn)
-print "RTh2 = %f" % RTh2
-print " "
+#print "RTh2 = %f" % RTh2
+#print " "
+
+# Now print the values to the output
+print TMPvIn
+print RTh1
+print RTh2
+#print('%(TMP) %(R1) %(R2)' % {'TMP': TMPvIn, 'R1': RTh1, 'R2': RTh2})
+#while(True):
+#    sleep(1)
