03892c22d62f2c51815e06b0885827401deffc92
romanomatthew23 <mmroman2@illinois.edu>
2016-11-03 20:07:34 -0500

    Update lavaLampPropControl.ino
    Fixed bug in color selecting switch statement.

--- lavaLampPropControl.ino
+++ lavaLampPropControl.ino
@@ -7,10 +7,10 @@
 const int trimPot = A5;//PC5 (A5 pin19) is the trimpot input
 const int button = 2;  //PD2 (pin2) is button input
 const int heat = 10;   //PB2 (pin10) is the heat PWM output
-const int led1 = 3;    //PD3 (pin3) is LED1 (assume is R) TODO: Check these assumptions
-const int led2 = 5;    //PD5 (pin5) is LED2 (assume is G)
-const int led3 = 6;    //PD6 (pin6) is LED3 (assume is B)
-const int led4 = 9;    //PB1 (pin9) is LED4 (assume is W)
+const int ledG = 3;    //PD3 (pin3) is LED1 (assume is G) TODO: Check these assumptions
+const int ledB = 5;    //PD5 (pin5) is LED2 (assume is B)
+const int ledR = 6;    //PD6 (pin6) is LED3 (assume is R)
+const int ledW = 9;    //PB1 (pin9) is LED4 (assume is W)
 const int tempHeat=23; //PC0 (pin23) is Heat Source Temp Sensor
 const int tempLamp=24; //PC1 (pin24) is Lava Lamp Temp Sensor
 
@@ -20,7 +20,7 @@ float maxHeatTemp = 85;   //Highest Allowable Heat Source Temp
 float K = 1;              //Proportional gain constant
 
 //Other Global Variables
-float potVal = 0;
+double potVal = 0;
 int buttonVal = 0;
 int oldButtonVal = 0;
 int mode = 0;     //modes 0 thru 4
@@ -39,11 +39,13 @@ float lampTemp = 25;  //lava lamp temp in Celsius
 ////////////////////////////////////////////////////////////////////////////
 void setup()
 {
+  //Serial.begin(9600);
+  
   pinMode(heat, OUTPUT);  //heat pwm output
-  pinMode(led1, OUTPUT);  //led pwm outputs
-  pinMode(led2, OUTPUT);
-  pinMode(led3, OUTPUT);
-  pinMode(led4, OUTPUT);
+  pinMode(ledG, OUTPUT);  //led pwm outputs
+  pinMode(ledB, OUTPUT);
+  pinMode(ledR, OUTPUT);
+  pinMode(ledW, OUTPUT);
   pinMode(button, INPUT); //button input 
 }
 
@@ -60,9 +62,9 @@ void loop()
   setLEDs();    //set LED PWM Signals;
   
   //Heat
-  tempInput();  //Read Temperature Sensors
-  calcHeat();   //Calculate Heat PWM Signals
-  setHeat();    //Set Heat PWM Signals
+  //tempInput();  //Read Temperature Sensors
+  //calcHeat();   //Calculate Heat PWM Signals
+  //setHeat();    //Set Heat PWM Signals
 }
 
 ////////////////////////////////////////////////////////////////////////////
@@ -78,9 +80,24 @@ void loop()
 //                                                                        //
 ////////////////////////////////////////////////////////////////////////////
 void userInput()
-{
+{   
   //read the dimmer value
-  potVal = analogRead(trimPot) / 1024;
+  potVal = analogRead(trimPot) / 1024.0;
+  /*
+  Serial.print("Pot Value: ");
+  Serial.print(potVal);
+  Serial.print("   mode: ");
+  Serial.print(mode);
+  Serial.print("   red: ");
+  Serial.print(redVal);
+  Serial.print("   blue: ");
+  Serial.print(blueVal);
+  Serial.print("   green: ");
+  Serial.print(greenVal);
+  Serial.print("   white: ");
+  Serial.print(whiteVal);
+  Serial.print("\n");
+  */
 
   //read the button value
   oldButtonVal = buttonVal;
@@ -100,40 +117,53 @@ void calcLEDs()
 {
   //obtain the color first
   switch(mode) {
-      case '0' :  //red
+      case 0:  //red
+         //Serial.print(" Red Mode\n");
          redVal = 255;
          greenVal = 0;
          blueVal = 0;
-         whiteVal = 255;
+         whiteVal = 0;
          break;
-      case '1' :  //green
+      case 1:  //green
+         //Serial.print(" Green Mode\n");
          redVal = 0;
          greenVal = 255;
          blueVal = 0;
-         whiteVal = 255;
-      case '2' : //blue
+         whiteVal = 0;
+         break;
+      case 2: //blue
+         //Serial.print(" Blue Mode\n");
          redVal = 0;
          greenVal = 0;
          blueVal = 255;
-         whiteVal = 255;
+         whiteVal = 0;
          break;
-      case '3' : //purple
+      case 3: //purple
+         //Serial.print(" Purple Mode\n");
          redVal = 255;
          greenVal = 0;
          blueVal = 255;
-         whiteVal = 255;
+         whiteVal = 0;
          break;
-      case '4' : //aqua
+      case 4: //aqua
+         //Serial.print(" Aqua Mode\n");
          redVal = 0;
          greenVal = 255;
          blueVal = 255;
-         whiteVal = 255;
+         whiteVal = 0;
          break;
+      default:
+         //Serial.print(" Default Mode\n");
+         redVal = 0;
+         greenVal = 0;
+         blueVal = 0;
+         whiteVal = 0;
    }
    //next adjust the values to be the correct overall intensity
    //TODO: May have to incorporate magnitude into it to correct for certain colors
       //having too much light from having more channels, etc.
    //float magnitude = sqrt(sq(redVal)+sq(greenVal)+sq(blueVal)+sq(whiteVal));
+   
    redVal = redVal*potVal;
    greenVal = greenVal*potVal;
    blueVal = blueVal*potVal;
@@ -147,10 +177,10 @@ void calcLEDs()
 ////////////////////////////////////////////////////////////////////////////
 void setLEDs() 
 {
-  analogWrite(led1,redVal);
-  analogWrite(led2,greenVal);
-  analogWrite(led3,blueVal);
-  analogWrite(led4,whiteVal);      
+  analogWrite(ledR,redVal);
+  analogWrite(ledG,greenVal);
+  analogWrite(ledB,blueVal);
+  analogWrite(ledW,whiteVal);      
 }
 
 ////////////////////////////////////////////////////////////////////////////
